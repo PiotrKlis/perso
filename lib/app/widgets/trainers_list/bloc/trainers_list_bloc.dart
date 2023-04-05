d6840b784defb6ebba77fec93d755e77028a378a
Piotr Klis <piotr.klis@onewelcome.com>
2023-04-06 00:35:56 +0200

    chore(android): update minSdkVersion to 21 refactor(trainers_list_bloc): change trainers repository from TestTrainersRepository to FirestoreTrainersRepository feat(firestore_trainers_repository): implement getTrainerCards method to fetch data from Firestore feat(pubspec.yaml): add cloud_firestore dependency

--- lib/app/widgets/trainers_list/bloc/trainers_list_bloc.dart
+++ lib/app/widgets/trainers_list/bloc/trainers_list_bloc.dart
@@ -1,20 +1,20 @@
 import 'package:Perso/app/models/trainer_card/trainer_card.dart';
 import 'package:Perso/app/widgets/trainers_list/event/trainers_list_event.dart';
 import 'package:Perso/app/widgets/trainers_list/state/trainers_list_state.dart';
-import 'package:Perso/data/repositories/trainers/test_trainers_repository.dart';
+import 'package:Perso/data/repositories/trainers/firestore_trainers_repository.dart';
 import 'package:Perso/data/repositories/trainers/trainers_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 class TrainersListBloc extends Bloc<TrainersListEvent, TrainersListState> {
   final TrainersRepository trainersProvider =
-      GetIt.I.get<TestTrainersRepository>();
+      GetIt.I.get<FirestoreTrainersRepository>();
 
   TrainersListBloc(TrainersListState initialState) : super(initialState) {
     on<LoadData>((event, emitter) async {
       try {
-      List<TrainerCard> trainers = await trainersProvider.getTrainerCards();
-      emitter(TrainersListState.content(trainers: trainers));
+        List<TrainerCard> trainers = await trainersProvider.getTrainerCards();
+        emitter(TrainersListState.content(trainers: trainers));
       } catch (error) {
         emitter(TrainersListState.error(error: error.toString()));
       }
