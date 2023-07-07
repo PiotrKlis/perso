40aafb4d1dac7292be3f8afbefa84e80a79bcd45
Piotr Klis <piotr.klis@onewelcome.com>
2023-07-07 02:09:31 +0200

    chore(search_screen.dart): organize imports and format code feat(search_screen.dart): add title and search bar to the search screen feat(search_screen.dart): add list view with separators to display search results

--- lib/app/screens/search/search_screen.dart
+++ lib/app/screens/search/search_screen.dart
@@ -1,5 +1,8 @@
 import 'package:Perso/app/utils/colors.dart';
+import 'package:Perso/app/utils/dimens.dart';
+import 'package:Perso/app/utils/theme_text.dart';
 import 'package:Perso/app/widgets/PersoAppBar.dart';
+import 'package:Perso/app/widgets/perso_divider.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -7,9 +10,50 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         backgroundColor: PersoColors.lightBlue,
         appBar: PersoAppBar(title: "Search screen"),
-        body: Center(child: Text("Search screen")));
+        body: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.only(
+                    top: Dimens.bigMargin,
+                    left: Dimens.normalMargin,
+                    right: Dimens.normalMargin),
+                child: Text(
+                  "Znajdź swojego wymarzonego trenera",
+                  style: ThemeText.smallTitleBold,
+                )),
+            Container(
+                margin: EdgeInsets.only(
+                    top: Dimens.bigMargin,
+                    left: Dimens.normalMargin,
+                    right: Dimens.normalMargin),
+                child: SearchBar()),
+            Container(
+                margin: EdgeInsets.only(top: Dimens.bigMargin),
+                child: Divider()),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  // Build your list item widget here
+                  return ListTile(
+                      title: Text(
+                    "[$index])",
+                    // Other ListTile properties...
+                  ));
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  // Build your separator widget here
+                  return Divider(
+                    color: Colors.grey,
+                    thickness: 1.0,
+                  );
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
