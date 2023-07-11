0ba48e103a7c94c949405cdcb66c6e19ee0d02ca
Piotr Klis <piotr.klis@onewelcome.com>
2023-07-12 00:27:38 +0200

    chore(training_categories.dart): organize imports and format code feat(training_categories.dart): update scaffold background color to white feat(training_categories.dart): update app bar title to "Training categories" feat(training_categories.dart): update body layout and add category list

--- lib/app/screens/training_categories/training_categories.dart
+++ lib/app/screens/training_categories/training_categories.dart
@@ -1,5 +1,7 @@
-import 'package:Perso/app/utils/colors.dart';
+import 'package:Perso/app/utils/dimens.dart';
+import 'package:Perso/app/utils/theme_text.dart';
 import 'package:Perso/app/widgets/PersoAppBar.dart';
+import 'package:Perso/app/widgets/training_category_list/perso_training_category_list.dart';
 import 'package:flutter/material.dart';
 
 class TrainingCategoriesScreen extends StatelessWidget {
@@ -7,9 +9,19 @@ class TrainingCategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: PersoColors.lightBlue,
+    return Scaffold(
+        backgroundColor: Colors.white,
         appBar: PersoAppBar(title: "Training categories"),
-        body: Center(child: Text("Training categories screen")));
+        body: Container(
+          margin: EdgeInsets.only(
+              top: Dimens.normalMargin, left: Dimens.normalMargin),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text("Categories", style: ThemeText.largerTitleBold),
+            Container(
+                margin: EdgeInsets.only(top: Dimens.bigMargin),
+                child: PersoTrainingCategoryList(isShortList: false))
+          ]),
+        ));
   }
 }
