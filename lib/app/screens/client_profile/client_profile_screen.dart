fd295888d4c7e20c216be36bee496c976ec8bf7f
lenartowskirafal <lenartowskirafal@gmail.com>
2023-09-14 20:01:27 +0200

    Extracted strings into applocalizations (app_en and app_pl )

--- lib/app/screens/client_profile/client_profile_screen.dart
+++ lib/app/screens/client_profile/client_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ClientProfileScreen extends StatelessWidget {
   const ClientProfileScreen({super.key});
@@ -6,7 +7,7 @@ class ClientProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Client profile screen"),
+      child: Text(AppLocalizations.of(context)!.profile_screen),
     );
   }
 }
