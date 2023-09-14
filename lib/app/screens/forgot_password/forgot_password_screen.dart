76eba2bf7e18a3f7e9513539aff8be9dd1b46ac7
lenartowskirafal <lenartowskirafal@gmail.com>
2023-09-14 20:39:27 +0200

    Extracted strings into applocalization (en/pl )

--- lib/app/screens/forgot_password/forgot_password_screen.dart
+++ lib/app/screens/forgot_password/forgot_password_screen.dart
@@ -8,6 +8,9 @@ import 'package:Perso/app/widgets/perso_button.dart';
 import 'package:Perso/app/widgets/perso_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'
+;
+
 
 class ForgotPasswordScreen extends StatelessWidget {
   ForgotPasswordScreen({super.key});
@@ -22,7 +25,7 @@ class ForgotPasswordScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: PersoColors.lightBlue,
         appBar: AppBar(
-          title: const Text('Password recovery'),
+          title: Text(AppLocalizations.of(context)!.password_recovery),
         ),
         body: SingleChildScrollView(
           keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
