5de7e2e2c89cf3eb8a12058f4e7631cd666fc4e8
lenartowskirafal <lenartowskirafal@gmail.com>
2023-10-05 11:02:06 +0200

    Merge branch 'main' into Applocalization
    # Conflicts:
    #	lib/app/screens/sign_in/sign_in_screen.dart
    #	lib/app/screens/sign_up/sign_up_screen.dart
    #	lib/app/screens/trainer_details/trainer_details_screen.dart
    #	localizations/app_en.arb
    #	localizations/app_pl.arb

--- lib/core/dependency_injection/get_it_config.config.dart
+++ lib/core/dependency_injection/get_it_config.config.dart
@@ -4,6 +4,10 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+// ignore_for_file: unnecessary_lambdas
+// ignore_for_file: lines_longer_than_80_chars
+// coverage:ignore-file
+
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
@@ -19,12 +23,10 @@ import 'package:Perso/data/trainers/firestore_trainers_service.dart' as _i6;
 import 'package:Perso/data/trainers/test_trainers_repository.dart' as _i11;
 import 'package:Perso/data/training_categories/local_training_category_repository.dart'
     as _i9;
-import 'package:Perso/data/user_info/user_info_provider.dart'
-    as _i12; // ignore_for_file: unnecessary_lambdas
+import 'package:Perso/data/user_info/user_info_provider.dart' as _i12;
 
-// ignore_for_file: lines_longer_than_80_chars
 extension GetItInjectableX on _i1.GetIt {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   _i1.GetIt init({
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
