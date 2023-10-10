d153779a431c3b618047e8d14da8212b298ab55b
lenartowskirafal <lenartowskirafal@gmail.com>
2023-10-10 20:14:22 +0200

    search filter screen - add dividers/ add chips widget/ applocalizations

--- lib/app/screens/search_filters/search_filter.dart
+++ lib/app/screens/search_filters/search_filter.dart
@@ -1,11 +1,13 @@
 import 'package:Perso/app/utils/colors.dart';
 import 'package:Perso/app/utils/dimens.dart';
 import 'package:Perso/app/utils/theme_text.dart';
+import 'package:Perso/app/widgets/category_chips/category_chips.dart';
 import 'package:Perso/app/widgets/perso_app_bar.dart';
 import 'package:Perso/app/widgets/perso_button.dart';
 import 'package:flutter/material.dart';
 import 'package:Perso/app/widgets/spoken_language_row.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SearchFilterScreen extends StatelessWidget {
   const SearchFilterScreen({super.key});
@@ -15,77 +17,96 @@ class SearchFilterScreen extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: PersoAppBar(
-          title: "Filters", 
+          title: AppLocalizations.of(context)!.filters,
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
+            Divider(
+              thickness: Dimens.dividerThickness
+            ),
             Container(
               margin: EdgeInsets.only(
-                top: 32,
+                top: 26,
                 left: 24,
               ),
               child: Text(
-                "Spoken Languages",
-                style: ThemeText.bodyBoldBlackText, 
+                AppLocalizations.of(context)!.spoken_languages,
+                style: ThemeText.bodyBoldBlackText,
               ),
             ),
             Container(
               margin: EdgeInsets.only(
                   top: Dimens.smallMargin, left: Dimens.normalMargin),
-              child: SpokenLanguageRowWidget(), 
+              child: SpokenLanguageRowWidget(),
+            ),
+            Divider(
+              thickness: Dimens.dividerThickness
             ),
             Container(
-              margin: EdgeInsets.only(top: 56, left: 24),
+              margin: EdgeInsets.only(top: 26, left: 24),
               child: Text(
-                "Location",
+                AppLocalizations.of(context)!.location,
                 style: ThemeText.bodyBoldBlackText,
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 24, top: 2),
+              margin: EdgeInsets.only(left: 24, top: 2, bottom: 26),
               child: Row(
                 children: [Text("By default it is set to My country")],
               ),
             ),
+            Divider(
+              thickness: Dimens.dividerThickness
+            ),
             Container(
-              margin: EdgeInsets.only(top: 58, left: 24),
+              margin: EdgeInsets.only(top: 26, left: 24),
               child: Row(
                 children: <Widget>[
-                  Text(
-                    "Rating",
-                    style: ThemeText.bodyBoldBlackText
-                    ),
+                  Text(AppLocalizations.of(context)!.rating, style: ThemeText.bodyBoldBlackText),
                   Container(
-                    margin: EdgeInsets.only(left: Dimens.bigMargin),
+                    margin: EdgeInsets.only(left: Dimens.bigMargin, bottom: 26),
                     child: RatingBar(
-                                allowHalfRating: true,
-                                  ratingWidget: RatingWidget(
-                      full: const Icon(Icons.star, color: Colors.yellow),
-                      half: const Icon(Icons.star_half, color: Colors.yellow),
-                      empty:
-                          const Icon(Icons.star_border, color: Colors.yellow)),
-                                  onRatingUpdate: (value) {
-                     //no-op
-                                  },
-                                 ),
+                      allowHalfRating: true,
+                      ratingWidget: RatingWidget(
+                          full: const Icon(Icons.star, color: Colors.yellow),
+                          half:
+                              const Icon(Icons.star_half, color: Colors.yellow),
+                          empty: const Icon(Icons.star_border,
+                              color: Colors.yellow)),
+                      onRatingUpdate: (value) {
+                        //no-op
+                      },
+                    ),
                   )
                 ],
               ),
+            ),
+            Divider(
+              thickness: Dimens.dividerThickness),
+            Container(
+                margin: EdgeInsets.only(top: 26, left: 24),
+                child: Text(
+                  AppLocalizations.of(context)!.specialities,
+                  style: ThemeText.bodyBoldBlackText,
+                )),
+            Container(
+              margin: EdgeInsets.only(top: Dimens.bigMargin, bottom: 26),
+              child: PersoCategoryChips(),
             ),
             Expanded(
               child: Align(
                 alignment: Alignment.bottomCenter,
                 child: Container(
-                  margin: EdgeInsets.only(bottom: 40),
+                  margin: EdgeInsets.only(bottom: 35),
                   child: PersoButton(
                     width: Dimens.bigButtonWidth,
-                    title: "Apply Filtres",
+                    title: AppLocalizations.of(context)!.apply_filtres_button,
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ));
   }
