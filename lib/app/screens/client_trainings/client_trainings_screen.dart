f8f2c940e67c6753e5709114d73beca672f8b588
Piotr Klis <piotr.klis@onewelcome.com>
2023-10-24 13:05:17 +0000

    feature/MAIN-T-22-client-trainings-no-trainers
    Merge branch 'main' into feature/MAIN-T-22-client-trainings-no-trainers
    
    # Conflicts:
    #	lib/core/navigation/navigation_config.dart
    
    Merge branch 'feature/MAIN-T-21-logged-out-training' into feature/MAIN-T-22-client-trainings-no-trainers
    
    # Conflicts:
    #	lib/core/navigation/navigation_config.dart
    
    Merge branch 'main' into feature/MAIN-T-21-logged-out-training
    
    # Conflicts:
    #	lib/app/screens/trainings/trainings_screen.dart
    #	lib/core/navigation/navigation_config.dart
    
    Merge branch 'feature/MAIN-T-21-logged-out-training' into feature/MAIN-T-22-client-trainings-no-trainers
    
    feat(logged_out_trainings_screen.dart): refactor widget structure and add separate classes for each widget
    feat(get_it.config.dart): add dependency injection configuration for main-scope dependencies
    
    refactor(client_trainings_screen.dart): remove unused imports and bloc provider
    
    chore(navigation_config.dart): update import paths for client_trainings and trainer_clients_list screens
    feat(client_trainings_screen.dart): create ClientTrainingsScreen widget
    feat(trainer_clients_list_screen.dart): create TrainerClientsScreen widget
    refactor(client_trainings.dart): delete unused ClientTrainingsScreen widget
    
    chore(logged_out_trainings_screen.dart): fix formatting and remove unnecessary comments and margins
    
    chore(theme_text.dart): remove unused largeTitleBoldWhite style
    refactor(navigation_config.dart): refactor redirect function to improve readability and remove unnecessary null return
    
    refactor(client_trainings.dart): update text in ClientTrainingsScreen to "Client trainings screen"
    refactor(trainer_clients_list.dart): update text in TrainerClientsScreen to "Trainer clients screen"
    
    feat: add client_trainings.dart, logged_out_trainings_screen.dart, and trainer_clients_list.dart screens
    
    This commit adds three new screens to the application:
    1. `client_trainings.dart`: This screen displays the client's trainings.
    2. `logged_out_trainings_screen.dart`: This screen is shown to logged-out users and displays information about finding trainers and personal plans.
    3. `trainer_clients_list.dart`: This screen displays the list of clients for a trainer.
    
    The screens are implemented using Flutter's `StatelessWidget` and contain basic UI elements such as text and buttons. The screens are added to the application's navigation configuration in `navigation_config.dart` using the corresponding screen navigation keys.
    
    Additionally, the `bottom_nav_bar.dart` file is modified to redirect the user to the appropriate screen based on their user type and login status. If the user is logged in as a trainer, they will be redirected to the `trainer_clients_list.dart` screen. If
    
    feat(trainings_screen.dart): add image and text to the training screen
    fix(theme_text.dart): add new text style for large title with white color
    
    
    Co-authored-by: Piotr Klis <piotr.klis@onewelcome.com>
    
    
    Merge-request: MAIN-MR-14
    Merged-by: Piotr Klis <piotr.klis0@gmail.com>

---
+++ lib/app/screens/client_trainings/client_trainings_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:perso/app/styleguide/value/app_colors.dart';
+import 'package:perso/app/styleguide/value/app_dimens.dart';
+import 'package:perso/app/styleguide/value/app_typography.dart';
+import 'package:perso/app/widgets/trainers_search_carousel/perso_trainers_search_carousel.dart';
+
+class ClientTrainingsScreen extends StatelessWidget {
+  const ClientTrainingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _NoTrainersView();
+  }
+}
+
+class _NoTrainersView extends StatelessWidget {
+  const _NoTrainersView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _Image(),
+            _Title(),
+            _TrainerSearchCarousel(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TrainerSearchCarousel extends StatelessWidget {
+  const _TrainerSearchCarousel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: Dimens.mMargin),
+      color: PersoColors.lightBlue,
+      child: Container(
+        margin: const EdgeInsets.only(
+          top: Dimens.mMargin,
+          bottom: Dimens.mMargin,
+        ),
+        child: PersoTrainersSearchCarousel(),
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        left: Dimens.mMargin,
+        top: Dimens.lMargin,
+        right: Dimens.mMargin,
+      ),
+      child: Text(
+        "You have no trainers so far. Let's change that.",
+        style: ThemeText.largeTitleBold,
+      ),
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: Dimens.substantialMargin,
+        left: Dimens.mMargin,
+        right: Dimens.mMargin,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(Dimens.buttonBorderRadius),
+        child: const Image(
+          image: AssetImage('assets/images/client_trainings_no_trainers.png'),
+        ),
+      ),
+    );
+  }
+}
