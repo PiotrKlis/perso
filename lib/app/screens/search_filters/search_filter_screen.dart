fd744748d821ab350f8bdaf739000c05c213cb13
lenartowskirafal <lenartowskirafal@gmail.com>
2023-11-01 14:50:02 +0100

    fixed searc_filter_scren

--- lib/app/screens/search_filters/search_filter_screen.dart
+++ lib/app/screens/search_filters/search_filter_screen.dart
@@ -1,14 +1,14 @@
-import 'package:Perso/app/utils/colors.dart';
-import 'package:Perso/app/utils/dimens.dart';
-import 'package:Perso/app/utils/theme_text.dart';
-import 'package:Perso/app/widgets/category_chips/category_chips.dart';
-import 'package:Perso/app/widgets/perso_app_bar.dart';
-import 'package:Perso/app/widgets/perso_button.dart';
+import 'package:perso/app/styleguide/value/app_dimens.dart';
+import 'package:perso/app/styleguide/value/app_typography.dart';
+import 'package:perso/app/widgets/category_chips/category_chips.dart';
+import 'package:perso/app/widgets/perso_app_bar.dart';
+import 'package:perso/app/widgets/perso_button.dart';
 import 'package:flutter/material.dart';
-import 'package:Perso/app/widgets/spoken_language_row.dart';
+import 'package:perso/app/widgets/spoken_language_row.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:Perso/app/widgets/perso_divider.dart';
+import 'package:perso/app/widgets/perso_divider.dart';
+
 
 class SearchFilterScreen extends StatelessWidget {
   const SearchFilterScreen({super.key});
@@ -74,7 +74,7 @@ class SearchFilterScreen extends StatelessWidget {
                       ratingWidget: RatingWidget(
                           full: const Icon(Icons.star, color: Colors.yellow),
                           half:
-                              const Icon(Icons.star_half, color: Colors.yellow),
+                          const Icon(Icons.star_half, color: Colors.yellow),
                           empty: const Icon(Icons.star_border,
                               color: Colors.yellow)),
                       onRatingUpdate: (value) {
