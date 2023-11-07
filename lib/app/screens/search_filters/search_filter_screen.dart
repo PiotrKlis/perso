4f7dfd42c7734ec03a4dd9a08715c6e5ea5e6a57
lenartowskirafal <lenartowskirafal@gmail.com>
2023-11-07 20:48:43 +0000

    filter_UI
    fix filter ui errors
    
    fixed searc_filter_scren
    
    Merge branch 'main' into filter_UI
    
    # Conflicts:
    #	lib/app/screens/home/home_screen.dart
    #	lib/app/screens/search_filters/search_filter.dart
    #	lib/core/dependency_injection/get_it_config.config.dart
    #	lib/core/navigation/navigation_config.dart
    
    no message
    
    Merge branch 'filter_UI' of https://github.com/PiotrKlis/perso into filter_UI
    
    no message
    
    no message
    
    fixup! fix names on filter screen
    fix names on filter screen
    - changed names in localizations
    
    fixup! fix renderflex on sign up screen
    fixup! fix renderflex on sign up screen
    fix renderflex on sign up screen
    
    - remove "for trainers" button and move GestureDetector on the right side
    
    Merge branch 'main' into filter_UI
    
    - remove "for trainers" button
    - move GestureDetector on the right side
    - fix mistaked on filter screen UI
    - fix names in folder localizations
    -changed name search_filter to search_filter_screen
    
    search filter- dimens
    
    Merge branch 'main' into filter_UI
    
    search_filter: fix margin
    
    search filter screen - add dividers/ add chips widget/ applocalizations
    
    Merge branch 'Applocalization' of https://github.com/PiotrKlis/perso into filter_UI
    
    # Conflicts:
    #	lib/app/screens/search_filters/search_filter.dart
    
    Merge branch 'Applocalization' of https://github.com/PiotrKlis/perso into filter_UI
    
    Merge
    
    Merge branch 'main' into Applocalization
    
    Merge branch 'main' into Applocalization
    
    # Conflicts:
    #	lib/app/screens/sign_in/sign_in_screen.dart
    #	lib/app/screens/sign_up/sign_up_screen.dart
    #	lib/app/screens/trainer_details/trainer_details_screen.dart
    #	localizations/app_en.arb
    #	localizations/app_pl.arb
    
    fixed code
    
    fixed code
    
    fix mistakes - deleted space and changed names
    
    maked row for rating
    
    implement filter screen UI - first steps/ created names/ created rating bar/ created Aplly Filtres button
    
    Merge branch 'main' into Applocalization
    
    # Conflicts:
    #	lib/app/screens/profile_edit/profile_edit_screen.dart
    #	lib/app/screens/sign_in/sign_in_screen.dart
    
    Checked some strings and extracted those app_en and app_pl - AppLocalizations functions
    
    Merge branch 'main' into Applocalization
    
    # Conflicts:
    #	lib/app/screens/client_profile/client_profile_screen.dart
    
    Merge branch 'main' into Applocalization
    
    Checked some strings and extracted those app_en and app_pl - AppLocalizations functions
    Fixed names of strings
    
    Checked some strings and extracted those app_en and app_pl - AppLocalizations functions
    
    Merge branch 'main' into Applocalization
    
    Checked some strings and extracted those app_en and app_pl - AppLocalizations functions
    
    
    Co-authored-by: Piotr Klis <piotr.klis0@gmail.com>
    
    
    Merge-request: MAIN-MR-9
    Merged-by: Rafał Lenartowski <lenartowskirafal@gmail.com>

---
+++ lib/app/screens/search_filters/search_filter_screen.dart
@@ -0,0 +1,118 @@
+import 'package:perso/app/styleguide/value/app_dimens.dart';
+import 'package:perso/app/styleguide/value/app_typography.dart';
+import 'package:perso/app/widgets/category_chips/category_chips.dart';
+import 'package:perso/app/widgets/perso_app_bar.dart';
+import 'package:perso/app/widgets/perso_button.dart';
+import 'package:flutter/material.dart';
+import 'package:perso/app/widgets/spoken_language_row.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:perso/app/widgets/perso_divider.dart';
+
+
+class SearchFilterScreen extends StatelessWidget {
+  const SearchFilterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PersoAppBar(
+          title: AppLocalizations.of(context)!.filters,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PersoDivider(),
+            Container(
+              margin: EdgeInsets.only(
+                top: Dimens.normalMargin,
+                left: Dimens.bigMargin,
+              ),
+              child: Text(
+                AppLocalizations.of(context)!.languages,
+                style: ThemeText.bodyBoldBlackText,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: Dimens.smallMargin, left: Dimens.normalMargin),
+              child: SpokenLanguageRowWidget(),
+            ),
+            PersoDivider(),
+            Container(
+              margin: EdgeInsets.only(
+                  top: Dimens.normalMargin, left: Dimens.bigMargin),
+              child: Text(
+                AppLocalizations.of(context)!.location,
+                style: ThemeText.bodyBoldBlackText,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  left: Dimens.bigMargin,
+                  top: Dimens.smallerMargin,
+                  bottom: Dimens.bigMargin),
+              child: Row(
+                children: [Text(AppLocalizations.of(context)!.location_default_set)],
+              ),
+            ),
+            PersoDivider(),
+            Container(
+              margin: EdgeInsets.only(
+                  top: Dimens.normalMargin,
+                  left: Dimens.bigMargin,
+                  bottom: Dimens.normalMargin),
+              child: Row(
+                children: [
+                  Text(AppLocalizations.of(context)!.rating,
+                      style: ThemeText.bodyBoldBlackText),
+                  Container(
+                    margin: EdgeInsets.only(left: Dimens.bigMargin),
+                    child: RatingBar(
+                      allowHalfRating: true,
+                      ratingWidget: RatingWidget(
+                          full: const Icon(Icons.star, color: Colors.yellow),
+                          half:
+                          const Icon(Icons.star_half, color: Colors.yellow),
+                          empty: const Icon(Icons.star_border,
+                              color: Colors.yellow)),
+                      onRatingUpdate: (value) {
+                        //no-op
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+            PersoDivider(),
+            Container(
+                margin: EdgeInsets.only(
+                    top: Dimens.normalMargin, left: Dimens.bigMargin),
+                child: Text(
+                  AppLocalizations.of(context)!.specialities,
+                  style: ThemeText.bodyBoldBlackText,
+                )),
+            Container(
+              margin: EdgeInsets.only(
+                  top: Dimens.bigMargin,
+                  left: Dimens.bigMargin,
+                  bottom: Dimens.bigMargin),
+              child: PersoCategoryChips(),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: EdgeInsets.only(bottom: Dimens.biggerMargin),
+                  child: PersoButton(
+                    width: Dimens.bigButtonWidth,
+                    title: AppLocalizations.of(context)!.confirm,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
