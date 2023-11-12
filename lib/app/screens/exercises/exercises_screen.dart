e7673928fe8e896a5d54d3d68281aacd198c9584
lenartowskirafal <lenartowskirafal@gmail.com>
2023-11-12 20:40:42 +0100

    exercise modify

--- lib/app/screens/exercises/exercises_screen.dart
+++ lib/app/screens/exercises/exercises_screen.dart
@@ -14,32 +14,75 @@ class ExercisesScreen extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             height: 350,
-            child: Card(child: Text("Exercises")),
+            child: Card(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    left: 16, top: 26,
+                  ),
+                    child: Text(
+              "Stop",
+              style: TextStyle(color: Colors.blue),
+            ))),
           ),
-          Container(
-            margin: EdgeInsets.only(
-              top: 48,
-            ),
-            child: Text(
-              "Exercise name",
-              style: ThemeText.bodyBoldBlackText,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 48,
+                ),
+                child: Text(
+                  "Exercise name",
+                  style: ThemeText.bodyBoldBlackText,
+                ),
+              ),
+            ],
           ),
-          Container(
-            margin: EdgeInsets.only(
-              top: 8,
-            ),
-            child: Text("12 reps"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 8,
+                ),
+                child: Text("12 reps"),
+              ),
+            ],
           ),
           Container(
             margin: EdgeInsets.only(
               top: 24,
-              left: 48,
-              right: 48,
+              left: Dimens.biggerMargin,
+              right: Dimens.biggerMargin,
             ),
             child: Text(
-                "Start standing with legs slightly wider than shoulder-distance Read More..."),
+                "Start standing with legs slightly wider than shoulder-distance Read More...", ),
           ),
+          Container(
+            margin: EdgeInsets.only(top: 100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Text("<", style: TextStyle(color: Colors.black54),),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left:40, right: 40),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 40,
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Text(">", style: TextStyle(color: Colors.black54),),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
