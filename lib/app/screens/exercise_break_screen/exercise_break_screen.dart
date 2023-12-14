ce5790e7974d68b98f1cb0dd5cae16c2d673329c
lenartowskirafal <lenartowskirafal@gmail.com>
2023-12-14 21:04:24 +0100

    no message

--- lib/app/screens/exercise_break_screen/exercise_break_screen.dart
+++ lib/app/screens/exercise_break_screen/exercise_break_screen.dart
@@ -15,79 +15,54 @@ class ExercisesBreakScreen extends StatelessWidget {
       backgroundColor: PersoColors.lightBlue,
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  top: 120,
-                ),
-                child: Image.asset(
-                  AppImages.restIcon,
-                  width: Dimens.restIconWidth,
-                  height: Dimens.restIconHeight,
-                ),
-              ),
-            ],
+          Container(
+            margin: const EdgeInsets.only(
+              top: 120,
+            ),
+            child: Image.asset(
+              AppImages.restIcon,
+              width: Dimens.restIconWidth,
+              height: Dimens.restIconHeight,
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  top: 48,
-                ),
-                child: const Text(
-                  'Rest',
-                  style: TextStyle(
-                    color: Color.fromRGBO(60, 60, 67, 0.60),
-                    fontSize: 17,
-                  ),
-                ),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 48,
+            ),
+            child: const Text(
+              'Rest',
+              style: TextStyle(
+                color: Color.fromRGBO(60, 60, 67, 0.60),
+                fontSize: 17,
               ),
-            ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 16),
-                child: const Text(
-                  '00:39',
-                  style: TextStyle(color: Colors.black, fontSize: 34),
-                ),
-              ),
-            ],
+          Container(
+            margin: const EdgeInsets.only(top: 16),
+            child: const Text(
+              '00:39',
+              style: TextStyle(color: Colors.black, fontSize: 34),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 35),
-                width: 322,
-                child: const PersoButton(
-                  title: '+20 sec',
-                ),
-              ),
-            ],
+          Container(
+            margin: const EdgeInsets.only(top: 35),
+            width: 322,
+            child: const PersoButton(
+              title: '+20 sec',
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 16),
-                child: const PersoButton(
-                  title: 'Stop',
-                  width: 322,
-                  whiteBlackTheme: true,
-                ),
-              ),
-            ],
+          Container(
+            margin: const EdgeInsets.only(top: 16),
+            child: const PersoButton(
+              title: 'Stop',
+              width: 322,
+              whiteBlackTheme: true,
+            ),
           ),
           Container(
             color: Colors.white,
             height: 100,
-            margin: const EdgeInsets.only(top: 120),
+            margin: const EdgeInsets.only(top: 100),
             child: Row(
               children: [
                 Container(
